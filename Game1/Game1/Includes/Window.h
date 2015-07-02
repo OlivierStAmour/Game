fcbf5135b11b38e1b7e0bb253294ff9173c6b762
Olivier St-Amour <olivierstamour0952@gmail.com>
2015-07-02 12:49:03 -0400

    Clean up

---
+++ Game1/Game1/Includes/Window.h
@@ -0,0 +1,44 @@
+#ifndef WINDOW
+#define WINDOW
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+/*Constant variables*/
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+
+class Window 
+{
+public:
+	//Initialize the variables
+	Window();
+	//Deallocates memory
+	~Window();
+
+	//Create the variables
+	bool init();
+	//Destroy the variables
+	void close();
+
+	//Loads a texture to the renderer
+	SDL_Texture* loadTexture(string path, int& width, int& height);
+
+	//Access to window
+	SDL_Window* getWindow();
+	//Access to renderer
+	SDL_Renderer* getRenderer();
+
+private:
+	SDL_Window* window_;
+	SDL_Renderer* renderer_;
+};
+
+
+#endif
